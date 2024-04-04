a454bf4725b731f0a2529f993fe64e315b425842
Robert Toth-Ludanyi <roberttothmails@yahoo.com>
2024-04-04 20:51:28 +0200

    Logs and comments removed

--- frontend/index.js
+++ frontend/index.js
@@ -5,22 +5,15 @@
 async function sprintChallenge5() { // Note the async keyword, in case you wish to use `await` inside sprintChallenge5
   // 👇 WORK WORK BELOW THIS LINE 👇
 
-  // fetcing learner data
-
   async function fetchLeanersData() {
     try {
       const response = await axios.get('http://localhost:3003/api/learners')
-
-      // console.log('Learners Data:', response.data)
-
       return response.data
     } catch (err) {
       console.log('Error fetching learnersData-->', err.message)
       return []
     }
   }
-
-  // fetching mentor data
 
   async function fetchMentorsData() {
     try {
@@ -34,8 +27,6 @@ async function sprintChallenge5() { // Note the async keyword, in case you wish 
   const learnersData = await fetchLeanersData()
   const mentorsData = await fetchMentorsData()
 
-  // adding metnor names to learner obj mentor array by id
-
   const learners = learnersData.map(learner => {
     return {
       ...learner,
@@ -45,17 +36,12 @@ async function sprintChallenge5() { // Note the async keyword, in case you wish 
     }
   })
 
-  // console.log('Learners after processing:', learners);
-
   const cardsContainer = document.querySelector('.cards')
   cardsContainer.innerHTML = ''
 
   let selectedCard = null
 
-  // make the learner cards
-
   learners.forEach(learner => {
-    // declaring variables
 
     const card = document.createElement('div')
     const name = document.createElement('h3')
@@ -63,15 +49,11 @@ async function sprintChallenge5() { // Note the async keyword, in case you wish 
     const mentor = document.createElement('h4')
     const mentorList = document.createElement('ul')
 
-    // adding class and text
-
     card.classList.add('card')
     mentor.classList.add('closed')
     name.textContent = `${learner.fullName}`
     email.textContent = learner.email
     mentor.textContent = 'Mentors'
-
-    // make the structure
 
     card.appendChild(name)
     card.appendChild(email)
@@ -88,55 +70,38 @@ async function sprintChallenge5() { // Note the async keyword, in case you wish 
 
     cardsContainer.appendChild(card)
 
-    // Event listener for when a card is clicked
-
     card.addEventListener('click', () => {
-      // Get the info element
       const infoElement = document.querySelector('.info')
 
-      // If there's a selected card and it's not the same as the current card
       if (selectedCard && selectedCard !== card) {
-        // Remove the 'selected' class from the previously selected card
         selectedCard.classList.remove('selected')
       }
 
-      // Toggle the 'selected' class on the current card
       card.classList.toggle('selected')
-      // Update the selectedCard variable to the current card
       selectedCard = card
 
-      // If the current card is selected
       if (card.classList.contains('selected')) {
-        // Display the learner's name and ID
         name.textContent = `${learner.fullName}, ID ${learner.id}`;
       } else {
-        // Display only the learner's name
         name.textContent = `${learner.fullName}`;
       }
 
-      // Update the info element text content based on whether a learner is selected
       infoElement.textContent = card.classList.contains('selected')
         ? `The selected learner is ${learner.fullName}`
         : "No learner is selected"
     })
 
-    // Event listener for when the mentor element is clicked
     mentor.addEventListener('click', () => {
-      // Get an array of mentor names from the learner object
       const mentorNames = learner.mentors.map(mentor => `${mentor.firstName} ${mentor.lastName}`)
-      // Generate display text based on whether mentors are assigned
       const displayText = mentorNames.length > 0
         ? mentorNames.join(', ')
         : "No mentors assigned"
-      // Set the text content of the mentorList element
       mentorList.textContent = displayText;
-      // Toggle the display style of the mentorList element
       mentorList.style.display = (mentorList.style.display === 'none')
         ? 'block'
         : 'none'
     })
 
-    // Get the info element and set its text content to indicate no learner is selected
     const infoElement = document.querySelector('.info');
     infoElement.textContent = "No learner is selected";
 
